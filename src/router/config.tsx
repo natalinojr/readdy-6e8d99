e2389f00421d548278b48a550ffa459a538031ea
natalinojr <146863866+natalinojr@users.noreply.github.com>
2026-08-13 15:49:45 -0300

    ZXGZDF

--- src/router/config.tsx
+++ src/router/config.tsx
@@ -57,7 +57,6 @@ const VoucherLinkPage = lazy(() => import('../pages/voucher-link/page'));
 const DeliveryPage = lazy(() => import('../pages/delivery/page'));
 const MotoboyPage = lazy(() => import('../pages/motoboy/page'));
 const MotoboyListaPage = lazy(() => import('../pages/motoboy-lista/page'));
-const _TesteMobileNav = lazy(() => import('../pages/tarefas/_TesteMobileNav'));
 
 const routes: RouteObject[] = [
   { path: '/login', element: <Login /> },
@@ -72,7 +71,6 @@ const routes: RouteObject[] = [
   { path: '/:storeSlug-delivery', element: <PullToRefresh><MobileKeyboardAssist /><DeliveryPage /></PullToRefresh> },
   { path: '/motoboy/:order_id', element: <PullToRefresh><MobileKeyboardAssist /><MotoboyPage /></PullToRefresh> },
   { path: '/entregas/:storeSlug', element: <PullToRefresh><MobileKeyboardAssist /><MotoboyListaPage /></PullToRefresh> },
-  { path: '/_teste-mobile-nav', element: <_TesteMobileNav /> },
   { path: '/autoatendimento', element: <AutoatendimentoPage /> },
   { path: '/totem/:token', element: <TotemPage /> },
   {
